e5c368f7a2591c0b37369c801852c5658dc9dcef
Anton Lukin <anton@lukin.me>
2016-09-02 23:39:52 +0300

    update likes

--- source/js/rambler.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-script.onload = init;
-script.async = true;
-script.src = 'https://developers.rambler.ru/likes/widget.js';
-document.head.appendChild(script);
